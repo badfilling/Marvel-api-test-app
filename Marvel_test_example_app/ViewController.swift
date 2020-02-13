2c54d8c8f6a56909c1abb299b4ed8cd515e5cda0
astepaniuk <artur.stepaniuk@comarch.pl>
2020-02-13 12:27:47 +0100

    Initial Commit

---
+++ Marvel_test_example_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Marvel_test_example_app
+//
+//  Created by Artur Stepaniuk on 13/02/2020.
+//  Copyright © 2020 Artur Stepaniuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
